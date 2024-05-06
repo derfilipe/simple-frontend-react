7b3db51d380a5ad29af541f39e2a7ae47dc324d5
derfilipe <f.andrade@planerio.de>
2024-05-06 18:45:12 +0200

    Removed unused imports

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,33 +1,14 @@
-import * as React from 'react';
-import {styled} from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
-import IconButton, {IconButtonProps} from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import {red} from '@mui/material/colors';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Grid} from "@mui/material";
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const {expand, ...other} = props;
-  return <IconButton {...other} />;
-})(({theme, expand}) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 const card1 = {
   title: "Kiwi",
@@ -74,21 +55,6 @@ export default function RecipeReviewCard() {
                     {card.info}
                   </Typography>
                 </CardContent>
-                {/*<CardActions disableSpacing>*/}
-                {/*  <ExpandMore*/}
-                {/*      expand={expanded}*/}
-                {/*      onClick={handleExpandClick}*/}
-                {/*      aria-expanded={expanded}*/}
-                {/*      aria-label="show more"*/}
-                {/*  >*/}
-                {/*    <ExpandMoreIcon/>*/}
-                {/*  </ExpandMore>*/}
-                {/*</CardActions>*/}
-                {/*<Collapse in={expanded} timeout="auto" unmountOnExit>*/}
-                {/*  <CardContent>*/}
-                {/*    <Typography paragraph>Method:</Typography>*/}
-                {/*  </CardContent>*/}
-                {/*</Collapse>*/}
               </Card>
             </Grid>
         ))}
